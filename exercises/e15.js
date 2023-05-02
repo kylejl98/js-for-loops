37580d027a2c68c149b56a4ec903ae9f6e61d194
Kyle Lejeune <kylejl98@gmail.com>
2023-05-02 19:21:44 -0400

    Passed test 15

--- exercises/e15.js
+++ exercises/e15.js
@@ -5,8 +5,18 @@
 // NOTE: You can NOT use the array.concat() method in your code
 
 export function concatArrays(arr1, arr2) {
-  // Your code goes here...
 
+  let concatArray = []
+
+  for (let i = 0; i < arr1.length; i++) {
+    concatArray.push(arr1[i]);
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    concatArray.push(arr2[i]);
+  }
+
+  return concatArray;
 }
 
 
